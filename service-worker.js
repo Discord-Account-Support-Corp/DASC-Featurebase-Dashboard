dda528fed4f212a034d1e94b7450ebaed84e2032
@UhLukas <119970521+UhLukas@users.noreply.github.com>
2026-05-21 14:39:51 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,7 @@
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-});
+self.addEventListener("install", () => self.skipWaiting());
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
+self.addEventListener("activate", () => self.clients.claim());
 
 self.addEventListener("fetch", (event) => {
-  // Only cache your own page
-  if (event.request.url.includes(self.location.origin)) {
-    event.respondWith(fetch(event.request));
-  }
+  event.respondWith(fetch(event.request));
 });
